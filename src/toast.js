2b0f863c180bf9627ee5472d5a80805d880f9a1d
Siyi <1203159664@qq.com>
2020-11-26 22:20:55 +0800

    Update toast.js

--- src/toast.js
+++ src/toast.js
@@ -1,12 +1,12 @@
 import toast from './pages/toast.vue'
-export default {
-    install(vue) {
-        let toastComponent = vue.extend(toast)
+export default {V
+    install(Vue) {
+        let toastComponent = Vue.extend(toast)
         const vm = new toastComponent(
             {el: document.createElement('div')}
         )
         document.getElementsByTagName('body')[0].appendChild(vm.$el)
-        vue.prototype.$toast = {
+        Vue.prototype.$toast = {
             showToast: function(msg) {
                 vm.showToast(msg)
             }
